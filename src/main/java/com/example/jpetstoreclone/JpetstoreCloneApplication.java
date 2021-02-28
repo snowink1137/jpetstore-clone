e5ca3be559bdcdbf5cee2791ac9a3c0ed47828f0
JJY <snowink1137@gmail.com>
2021-02-28 22:39:19 +0900

    make project

---
+++ src/main/java/com/example/jpetstoreclone/JpetstoreCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpetstoreclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetstoreCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpetstoreCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpetstoreclone/JpetstoreCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpetstoreclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpetstoreCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
